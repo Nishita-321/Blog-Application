ba8904b9c421f80dd6275c4485ed8aeb4cded9d5
Nishita <nishitagarg027@gmail.com>
2025-10-30 15:32:33 +0530

    my blogs added

--- assets/js/blogs.js
+++ assets/js/blogs.js
@@ -1,11 +1,11 @@
 let allBlog = JSON.parse(localStorage.getItem("allBlogs")) || [];
+const currentUser = JSON.parse(localStorage.getItem("currentUser"));
 
 function saveBlog() {
+  let pic = document.getElementById("pic");
   let title = document.getElementById("title");
   let tags = document.getElementById("tags");
   let para = document.getElementById("para");
-
-  let currentUser = JSON.parse(localStorage.getItem("currentUser"));
 
   if(!currentUser) {
     alert("Please Log in!");
@@ -15,6 +15,7 @@ function saveBlog() {
      
  
   let obj = {
+    pic : pic.value,
     title: title.value,
     tags: tags.value,
     likes: [],
@@ -22,7 +23,7 @@ function saveBlog() {
     user : currentUser.username
   };
 
-  if(!obj.title || !obj.tags || !obj.likes || !obj.para){
+  if(!obj.title || !obj.tags || !obj.likes || !obj.para || !obj.pic){
     alert("Fill all details!!");
     return;
   }
@@ -33,4 +34,31 @@ function saveBlog() {
 
   alert("Your Blog has been saved!!")
 
+}
+
+function loadMyBlogs() {
+
+  if(!currentUser) {
+    alert("You are not logged in!!");
+    window.location.href = "login.html";
+    return;
+  }
+
+  for(let i = 0; i < allBlog.length; i++){
+    if(allBlog[i].user == currentUser.username){
+      document.getElementById("myBlogContainer").innerHTML += `
+      <div class="MyBlogscard">
+                <img class="MyBlogscardImg" src="${allBlog[i].pic}" alt="car">
+                <div>
+                    <p class="MyBlogscardTitle">${allBlog[i].title}</p>
+                    <p class="MyBlogscardTags">Tags: ${allBlog[i].tags}</p>
+                    <p class="MyBlogscardLikes">Likes: ${allBlog[i].likes.length}</p>
+                </div>
+                <div class="MyBlogscardBtns">
+                    <button>Delete</button>
+                    <button>Edit</button>
+                </div>
+            </div>`
+    }
+  }
 }
